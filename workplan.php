ea9f0740dd95cedae03a92e5445399642e24a3f8
Gabriela Diniz <gadisa10@outlook.com>
2022-11-04 15:00:52 -0300

    Criando borda animada.

--- workplan.php
+++ workplan.php
@@ -1,6 +1,6 @@
 <?php
 
-    $run = shell_exec("\\10.10.0.219\Worklan Selenium\Workplan.py");
+    $run = shell_exec("\\10.10.0.219\WorklanSelenium\Workplan.py");
     header("location:indexErro.html");
     
 ?>
